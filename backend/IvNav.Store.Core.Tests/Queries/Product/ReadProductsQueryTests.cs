fd7c5a0b17f13b0cbeadc586594f781a3fe5a793
Ivan Navalotski <ivan.navalotski@gmail.com>
2023-04-06 01:20:25 +0200

    add tests

---
+++ backend/IvNav.Store.Core.Tests/Queries/Product/ReadProductsQueryTests.cs
@@ -0,0 +1,59 @@
+using IvNav.Store.Core.Queries.Product;
+using IvNav.Store.Core.Tests.Helpers;
+using NUnit.Framework;
+using IvNav.Store.Core.Tests.Helpers.EntityBuilders;
+
+namespace IvNav.Store.Core.Tests.Queries.Product;
+
+[TestFixture]
+internal class ReadProductsQueryTests : DatabaseFixture
+{
+    private readonly ReadProductsQuery _query;
+
+    public ReadProductsQueryTests()
+    {
+        _query = new ReadProductsQuery(ApplicationDbContext);
+    }
+
+    [Test]
+    public async Task When_ReadProductQuery_Expect_Value()
+    {
+        // arrange
+        var products = new List<Infrastructure.Entities.Product>
+        {
+            await this.CreateProduct(),
+            await this.CreateProduct()
+        };
+
+        var request = new ReadProductsRequest(null, null);
+
+        // act
+        var response = await _query.Handle(request, default);
+
+        // assert
+        Assert.IsNotNull(response);
+        Assert.IsNotNull(response.Items);
+
+        for (var i = 0; i < products.Count; i++)
+        {
+            Assert.AreEqual(products[i].Id, response.Items.ElementAt(i).Id);
+            Assert.AreEqual(products[i].Name, response.Items.ElementAt(i).Name);
+        }
+    }
+
+    [Test]
+    public async Task When_ReadProductQuery_Expect_Null()
+    {
+        // arrange
+        var request = new ReadProductsRequest(null, null);
+
+        // act
+        var response = await _query.Handle(request, default);
+
+        // assert
+        Assert.IsNotNull(response);
+        Assert.IsNotNull(response.Items);
+        Assert.AreEqual(0, response.Items.Count);
+    }
+}
+
